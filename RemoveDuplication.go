f056aab52705c80650319e0e36dd1ce464c7f5e3
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-09 15:32:30 -0700

    :memo: solve: 80: Remove Duplicates from Sorted Array II

--- RemoveDuplication.go
+++ RemoveDuplication.go
@@ -13,3 +13,26 @@ func removeDuplicates(nums []int) int {
 	}
 	return len(hold)
 }
+
+func removeDuplicatesII(nums []int) int {
+	var hold []int
+	hold = append(hold, nums[0])
+	k := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1] {
+			k++
+		} else {
+			k = 0
+		}
+		if k >= 2 {
+			continue
+		} else {
+			hold = append(hold, nums[i])
+		}
+	}
+	for i := range hold {
+		nums[i] = hold[i]
+	}
+
+	return len(hold)
+}
